8747db4b3c7f6af0a85dc671453f742cca2a491b
Ard <ard4foetsie@gmail.com>
2020-06-06 20:37:54 +0200

    int
    int

---
+++ discordbot.py
@@ -0,0 +1,30 @@
+import discord
+import requests
+import json
+
+# create discord client
+client = discord.Client()
+
+# token
+bot.login(proces.env.token);
+
+# bot is ready
+@client.event
+async def on_ready():
+	try:
+		# print bot information
+		print(client.user.name)
+		print(client.user.id)
+		print('Discord.py Version: {}'.format(discord.__version__))
+	
+	except Exception as e:
+		print(e)
+
+# on new message
+@client.event
+async def on_message(message):
+	# print message content
+	await message.content
+
+# start bot
+client.run(token)
