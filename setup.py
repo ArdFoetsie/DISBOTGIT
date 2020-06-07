0caa2fc5689eab05d13db933679b972dc74cce5a
Ard <ard4foetsie@gmail.com>
2020-06-07 18:58:11 +0200

    3e
    3e

--- setup.py
+++ setup.py
@@ -6,26 +6,9 @@ import os
 # create discord client
 client = discord.Client()
 
-# token
-
-token = os.environ["token"]
-# bot is ready
 @client.event
 async def on_ready():
-	try:
-		# print bot information
-		print(client.user.name)
-		print(client.user.id)
-		print('Discord.py Version: {}'.format(discord.__version__))
-	
-	except Exception as e:
-		print(e)
 
-# on new message
-@client.event
-async def on_message(message):
-	# print message content
-	await message.content
+print("Bot is ready")
 
-# start bot
-client.run(token)
+client.run(os.environ['token'])
